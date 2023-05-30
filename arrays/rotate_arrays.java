7ee0e67d7f4e1a0852abd00cd2f6b8a231289bb1
manish kakoti <manishk@smokescreen.io>
2023-05-30 08:44:59 +0530

    more solutions

---
+++ arrays/rotate_arrays.java
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/rotate-array/description/
+// O(N^2) solution
+class Solution {
+    public void rotate(int[] nums, int k) {
+        for (int i=0;i<k;i++) {
+            int temp = nums[nums.length-1];
+            for (int j=nums.length-1;j>0;j--) {
+                nums[j] = nums[j-1];
+            }
+            nums[0] = temp;
+        }
+    }
+}
+
+// improved solution
+// O(n) solution
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k%=nums.length;
+        reverse(nums, 0, nums.length-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, nums.length-1);
+    }
+
+    public void reverse(int [] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
