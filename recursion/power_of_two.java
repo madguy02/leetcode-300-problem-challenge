da6276308b455e5575aef74e24bbd224f212229d
madguy02 <manishkakoti688@gmail.com>
2023-06-12 14:23:51 +0530

    new problem solution

---
+++ recursion/power_of_two.java
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/power-of-two
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+
+       if (n==0) {
+           return false;
+       }
+
+       if (n==1) {
+           return true;
+       }
+
+       if (n%2==0 && isPowerOfTwo(n/2)) {
+           return true;
+       }
+
+       return false;
+    }
+
+  
+}
